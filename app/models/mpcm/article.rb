b71d0e05ec6eef83b8204beb58fb83a52f0f97f4
guillaume bonin <guillaume.bonin@seniormedia.fr>
2017-01-18 17:32:44 +0100

    Add show view and modify a bit other view, add method in Article model

--- app/models/mpcm/article.rb
+++ app/models/mpcm/article.rb
@@ -1,5 +1,45 @@
 module Mpcm
   class Article < ApplicationRecord
-  	scope :published, -> { where('published_at <= ?', Time.zone.now) }
+    scope :published, -> { where('published_at <= ?', Time.zone.now) }
+
+    def formatted_height
+      if self.height
+        "#{self.height} cm"
+      else
+        'N/C'
+      end
+    end
+
+    def formatted_width
+      if self.width
+        "#{self.width} cm"
+      else
+        'N/C'
+      end
+    end
+
+    def formatted_depth
+      if self.depth
+        "#{self.depth} cm"
+      else
+        'N/C'
+      end
+    end
+
+    def formatted_weigth
+      if self.weigth
+        "#{self.weigth} kg"
+      else
+        'N/C'
+      end
+    end
+
+    def formatted_price
+      if self.price
+        "#{self.price} €"
+      else
+        'N/C'
+      end
+    end
   end
 end
--- mpcm.gemspec
+++ mpcm.gemspec
@@ -17,6 +17,7 @@ Gem::Specification.new do |s|
   s.files = Dir["{app,config,db,lib}/**/*", 'MIT-LICENSE', 'Rakefile', 'README.md']
 
   s.add_dependency 'rails', '~> 5.0.1'
+  s.add_dependency 'slim-rails', '~> 3.1.1'
   s.add_dependency 'bootstrap-sass', '~> 3.2.0'
   s.add_dependency 'simple_form', '~> 3.4.0'
 
